0c828c2fe812630fa9bac93c13543a56202c0908
OrigamiDream <crystal_vc31@naver.com>
2019-06-29 18:13:57 +0900

    Initial Commit

---
+++ ProDisplayXDR/ProDisplayXDRView.h
@@ -0,0 +1,13 @@
+//
+//  ProDisplayXDRView.h
+//  ProDisplayXDR
+//
+//  Created by OrigamiDream on 29/06/2019.
+//  Copyright © 2019 Avis Studio. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ProDisplayXDRView : ScreenSaverView
+
+@end
---
+++ ProDisplayXDR/ProDisplayXDRView.m
@@ -0,0 +1,52 @@
+//
+//  ProDisplayXDRView.m
+//  ProDisplayXDR
+//
+//  Created by OrigamiDream on 29/06/2019.
+//  Copyright © 2019 Avis Studio. All rights reserved.
+//
+
+#import "ProDisplayXDRView.h"
+
+@implementation ProDisplayXDRView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
